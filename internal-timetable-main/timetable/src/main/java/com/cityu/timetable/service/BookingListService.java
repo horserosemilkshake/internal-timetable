d3c0764815600e52f15e786a4ed1f828a8e1b064
horserosemilkshake <61784292+horserosemilkshake@users.noreply.github.com>
2022-07-18 11:02:36 +0800

    Delete internal-timetable-main/timetable directory

--- internal-timetable-main/timetable/src/main/java/com/cityu/timetable/service/BookingListService.java
+++
@@ -1,8 +0,0 @@
-package com.cityu.timetable.service;
-
-import com.cityu.timetable.entity.BookingList;
-
-public interface BookingListService {
-    abstract BookingList getBookingByMonth(String room_id, String month);
-    abstract String getAllBooking(String room_id);
-}
--- internal-timetable-main/timetable/src/main/java/com/cityu/timetable/service/TimeTableService.java
+++
@@ -1,9 +0,0 @@
-package com.cityu.timetable.service;
-
-import com.cityu.timetable.entity.TimeTable;
-
-import java.util.List;
-
-public interface TimeTableService {
-    abstract TimeTable provideChart(List<String> rooms, String month);
-}

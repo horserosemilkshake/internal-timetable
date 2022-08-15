2a3f8931b8988a1e1f66d1c7dcf8f3e4bc00efe2
Eric TSOI Tsz Chun <61784292+horserosemilkshake@users.noreply.github.com>
2022-08-15 10:32:08 +0800

    Delete timetable directory

--- timetable/src/main/java/com/cityu/timetable/service/BookingListService.java
+++
@@ -1,8 +0,0 @@
-package com.cityu.timetable.service;
-
-import com.cityu.timetable.entity.BookingList;
-
-public interface BookingListService {
-    BookingList getBookingByMonth(String room_id, String month);
-    String getAllBooking(String room_id);
-}
--- timetable/src/main/java/com/cityu/timetable/service/ChartService.java
+++
@@ -1,8 +0,0 @@
-package com.cityu.timetable.service;
-
-import com.cityu.timetable.entity.BookingList;
-import com.cityu.timetable.entity.Chart;
-
-public interface ChartService {
-    Chart provideChart(BookingList bookings, String month);
-}

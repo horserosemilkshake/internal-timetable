d0e7079a5c050b3190899d058d1f8e9e2f49f110
Eric TSOI Tsz Chun <61784292+horserosemilkshake@users.noreply.github.com>
2022-08-17 15:07:32 +0800

    Add files via upload

---
+++ timetable-deploy/src/main/java/com/cityu/timetable/TimeTableApplication.java
@@ -0,0 +1,19 @@
+package com.cityu.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TimeTableApplication extends SpringBootServletInitializer{
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTableApplication.class, args);
+    }
+    /*
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TimeTableApplication.class);
+    }*/
+}
+
